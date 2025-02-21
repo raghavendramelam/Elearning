dfaf1b0d6f9d4a50e6e495a27b257c9b58d68985
Raghavendra Melam <150319134+raghavendramelam@users.noreply.github.com>
2025-02-21 17:10:13 -0600

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,6 +2,6 @@ public class demo{
     public static void main(String[] args) {
         System.out.println("Hello good morning welcome to java");
 
-        System.out.println(" How are you ");
+        System.out.println(" How are you  mam");
     }
 }
